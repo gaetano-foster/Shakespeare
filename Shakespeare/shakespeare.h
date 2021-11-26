28ca533cdc8333f823ce1204c508531c1656986f
Gaetano Foster <gaetanox109@gmail.com>
2021-11-25 21:55:26 -0500

    Created shakespeare library

---
+++ Shakespeare/shakespeare.h
@@ -0,0 +1,61 @@
+#ifndef SHAKE_H
+#define SHAKE_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#define and_end_this_dreadful_line ;
+#define begin_thy_orders {
+#define bring_thy_tyrannical_reign_to_an_end }
+
+#define abyss void
+#define numeral int
+#define fractional_numeral float
+#define large_fractional_numeral double
+#define large_numeral long
+#define positive_numeral unsigned int
+#define positive_fractional_numeral unsigned float
+#define positive_large_fractional_numeral unsigned double
+#define positive_large_numeral unsigned long
+#define unsure_numeral signed int
+#define unsure_fractional_numeral signed float
+#define unsure_large_fractional_numeral signed double
+#define unsure_large_numeral signed long
+
+#define you_would_not_happen_to_have_a_bit_of_memory_on_you(a_bit) malloc(a_bit)
+#define please_do_clear_a_bit_of_memory_on_you(a_bit) malloc(a_bit)
+#define here_ye__I_shall_return_thy_memory(bit) free(bit)
+#define copy_the_following(...) printf(__VA_ARGS__) 
+
+#define shall_be_set_to =
+#define added_to +
+#define without_the_following -
+#define instances_of *
+#define seperated_into /
+#define naught 0
+#define fact bool
+#define a_statement_of_deception false
+#define a_statement_of_truth true
+#define doth_be ==
+#define doth_not_be !=
+#define greater_than >
+#define lesser_than <
+#define greater_than_or_equal_to >=
+#define lesser_than_or_equal_to <=
+#define escape_this_loop break
+#define iterate_while while
+#define do_for_each_time for
+
+#define letter char
+#define wide_letter wchar_t
+#define positive_letter unsigned char
+#define positive_yet_wide_letter unsigned char
+
+#define a_value_that_directs_itself_toward_ &
+#define the_value_pointed_to_by_ *
+#define a_value_that_directs_itself_toward_ *
+
+#define alias_of typedef
+#define data_collection struct
+
+#endif
